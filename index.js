a6c738b3ee125f1c7fd6f5886b68da00eacdffe3
mudit raj sade <muditrajsade89@gmail.com>
2025-01-22 12:03:04 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let path = require('path');
+let cors = require('cors');
+
+
+app.use(
+    cors({
+        origin: '*',
+    })
+);
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/',function(req,res){
+    res.json({message:"hello"})
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
